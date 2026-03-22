1412ce651b76249a1a9e2aaed0600800d5734a06
yeoeol <gnsl3528@naver.com>
2026-03-22 17:16:47 +0900

    first commit

---
+++ src/main/java/com/commerce/pagopa/PagopaApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.pagopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagopaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagopaApplication.class, args);
+    }
+
+}
