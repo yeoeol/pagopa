6d8b022d7fb4aa606a9fddc6a0801c412e98ea7e
yeoeol <gnsl3528@naver.com>
2026-04-23 15:29:42 +0900

    refactor: OrderResponseDto 수정

--- src/main/java/com/commerce/pagopa/domain/order/dto/response/OrderResponseDto.java
+++ src/main/java/com/commerce/pagopa/domain/order/dto/response/OrderResponseDto.java
@@ -1,6 +1,7 @@
 package com.commerce.pagopa.domain.order.dto.response;
 
 import com.commerce.pagopa.domain.order.entity.Order;
+import com.commerce.pagopa.domain.order.entity.enums.OrderStatus;
 import com.commerce.pagopa.domain.order.entity.enums.PaymentMethod;
 import com.commerce.pagopa.domain.user.dto.response.UserResponseDto;
 
@@ -11,18 +12,18 @@ public record OrderResponseDto(
         Long orderId,
         String orderNumber,
         BigDecimal totalAmount,
-        String status,
+        OrderStatus status,
         PaymentMethod paymentMethod,
         UserResponseDto user,
         DeliveryResponseDto delivery,
-        List<OrderProductResponseDto> orderItems
+        List<OrderProductResponseDto> orderProducts
 ) {
     public static OrderResponseDto from(Order order) {
         return new OrderResponseDto(
                 order.getId(),
                 order.getOrderNumber(),
                 order.getTotalAmount(),
-                order.getStatus().getDescription(),
+                order.getStatus(),
                 order.getPaymentMethod(),
                 UserResponseDto.from(order.getUser()),
                 DeliveryResponseDto.from(order.getDelivery()),
