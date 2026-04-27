cda9a8de281f26cf1ec36728175727fe83f0f0ac
yeoeol <gnsl3528@naver.com>
2026-04-27 17:38:00 +0900

    fix: 검색조건 수정

--- src/main/java/com/commerce/pagopa/domain/product/dto/request/ProductSearchCondition.java
+++ src/main/java/com/commerce/pagopa/domain/product/dto/request/ProductSearchCondition.java
@@ -1,11 +1,6 @@
 package com.commerce.pagopa.domain.product.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 public record ProductSearchCondition(
-        @NotBlank(message = "{validation.notBlank}")
-        @Size(max = 100, message = "{validation.size}")
         String productName        // 상품명 검색
 ) {
 }
