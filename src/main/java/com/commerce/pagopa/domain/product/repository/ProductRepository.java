9c899878d2ba15fd11dd80aeea5891c5f4881287
yeoeol <gnsl3528@naver.com>
2026-04-27 23:58:25 +0900

    fix: 재고 증가, 감소 Lazy 로딩 문제 해결

--- src/main/java/com/commerce/pagopa/domain/product/repository/ProductRepository.java
+++ src/main/java/com/commerce/pagopa/domain/product/repository/ProductRepository.java
@@ -5,36 +5,10 @@ import com.commerce.pagopa.domain.product.entity.enums.ProductStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
-    @Modifying(clearAutomatically = true)
-    @Query(value =
-            "UPDATE Product p " +
-            "SET p.stock = p.stock + :quantity " +
-            "WHERE p.id = :productId " +
-                    "AND :quantity > 0")
-    int increaseStock(@Param("productId") Long productId, @Param("quantity") int quantity);
-
-    @Modifying(clearAutomatically = true)
-    @Query(value =
-            "UPDATE Product p " +
-            "SET p.stock = p.stock - :quantity " +
-            "WHERE p.id = :productId " +
-                    "AND :quantity > 0 " +
-                    "AND p.stock >= :quantity")
-    int decreaseStock(@Param("productId") Long productId, @Param("quantity") int quantity);
-
-    @Query("SELECT p FROM Product p " +
-           "WHERE (:name IS NULL " +
-                    "OR :name = '' " +
-                    "OR p.name LIKE CONCAT('%', :name, '%'))")
-    List<Product> searchProducts(@Param("name") String name);
 
     Page<Product> findAllBySellerId(Long userId, Pageable pageable);
 
