5f7f46e9be297c585038bb38cc3a79a6ac5482bf
yeoeol <gnsl3528@naver.com>
2026-04-30 01:19:43 +0900

    chore: 하드코딩 환경변수화

--- src/main/java/com/commerce/pagopa/domain/order/scheduler/OrderScheduler.java
+++ src/main/java/com/commerce/pagopa/domain/order/scheduler/OrderScheduler.java
@@ -6,6 +6,7 @@ import com.commerce.pagopa.domain.order.repository.OrderRepository;
 import com.commerce.pagopa.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +21,16 @@ public class OrderScheduler {
     private final OrderRepository orderRepository;
     private final OrderService orderService;
 
+    @Value("${app.order.unpaid-before-min}")
+    private Long beforeMin;
+
     /**
      * 1분마다 실행되며, 주문 생성 후 15분이 지나도록 결제되지 않은(ORDERED) 주문을 찾아 자동 취소합니다.
      */
     @Scheduled(cron = "0 * * * * *") // 매 분 0초에 실행
     public void cancelUnpaidOrders() {
         // 15분 이전 시간 계산
-        LocalDateTime timeoutTime = LocalDateTime.now().minusMinutes(15);
+        LocalDateTime timeoutTime = LocalDateTime.now().minusMinutes(beforeMin);
         
         // 상태가 ORDERED 이면서 생성 시간이 15분 이하(정확히 15분 전 포함)인 주문들 조회
         List<Order> unpaidOrders = orderRepository.findByStatusAndCreatedAtLessThanEqual(OrderStatus.ORDERED, timeoutTime);
