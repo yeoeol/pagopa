ee961815f4e2acd084d2bfc13aa06ac4b74b6806
yeoeol <gnsl3528@naver.com>
2026-05-06 16:59:33 +0900

    feat: SearchHistoryController Swagger Annotations 추가

--- src/main/java/com/commerce/pagopa/searchhistory/presentation/SearchHistoryController.java
+++ src/main/java/com/commerce/pagopa/searchhistory/presentation/SearchHistoryController.java
@@ -5,6 +5,8 @@ import com.commerce.pagopa.global.response.ApiResponse;
 import com.commerce.pagopa.global.util.CookieUtil;
 import com.commerce.pagopa.searchhistory.application.SearchHistoryService;
 import com.commerce.pagopa.searchhistory.application.dto.response.SearchHistoryResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "SEARCH HISTORY API", description = "검색 기록 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/search-histories")
@@ -21,6 +24,7 @@ public class SearchHistoryController {
 
     private final SearchHistoryService searchHistoryService;
 
+    @Operation(summary = "검색 기록 조회", description = "검색 기록을 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<SearchHistoryResponseDto>>> getHistories(
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -34,6 +38,7 @@ public class SearchHistoryController {
         );
     }
 
+    @Operation(summary = "검색 기록 단건 삭제", description = "검색 기록을 단건 삭제합니다.")
     @DeleteMapping("/{id}")
     @PreAuthorize("@searchHistoryOwnerValidator.isOwner(#searchHistoryId, #userDetails != null ? #userDetails.userId : null)")
     public ResponseEntity<ApiResponse<Void>> deleteHistory(
@@ -44,6 +49,7 @@ public class SearchHistoryController {
         return ResponseEntity.ok(ApiResponse.ok());
     }
 
+    @Operation(summary = "검색 기록 전체 삭제", description = "검색 기록을 전체 삭제합니다.")
     @DeleteMapping
     public ResponseEntity<ApiResponse<Void>> deleteAllHistories(
             @AuthenticationPrincipal CustomUserDetails userDetails,
