d982d388517baa48d566f347ed1bf0b7a15caa5f
yeoeol <gnsl3528@naver.com>
2026-05-06 16:56:11 +0900

    feat: ScrapController Swagger Annotations 추가

--- src/main/java/com/commerce/pagopa/scrap/presentation/ScrapController.java
+++ src/main/java/com/commerce/pagopa/scrap/presentation/ScrapController.java
@@ -5,6 +5,8 @@ import com.commerce.pagopa.global.response.ApiResponse;
 import com.commerce.pagopa.scrap.application.ScrapService;
 import com.commerce.pagopa.scrap.application.dto.request.ScrapAddRequestDto;
 import com.commerce.pagopa.scrap.application.dto.response.ScrapResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "SCRAP API", description = "스크랩 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/scraps")
@@ -22,6 +25,7 @@ public class ScrapController {
 
     private final ScrapService scrapService;
 
+    @Operation(summary = "스크랩 추가", description = "특정 게시글을 스크랩합니다.")
     @PostMapping
     public ResponseEntity<ApiResponse<ScrapResponseDto>> addScrap(
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -34,6 +38,7 @@ public class ScrapController {
                 );
     }
 
+    @Operation(summary = "스크랩 삭제", description = "스크랩을 삭제합니다.")
     @DeleteMapping("/{id}")
     @PreAuthorize("@scrapOwnerValidator.isOwner(#scrapId, principal.userId)")
     public ResponseEntity<ApiResponse<Void>> deleteScrap(@PathVariable("id") Long scrapId) {
@@ -43,6 +48,7 @@ public class ScrapController {
         );
     }
 
+    @Operation(summary = "스크랩 목록 조회", description = "스크랩 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<ScrapResponseDto>>> getScraps(
             @AuthenticationPrincipal CustomUserDetails userDetails
