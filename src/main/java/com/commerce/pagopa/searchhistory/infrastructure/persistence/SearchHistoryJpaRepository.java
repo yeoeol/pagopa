fb9e074c70a178e226df42bb47c601e82b05e146
yeoeol <gnsl3528@naver.com>
2026-05-30 17:06:09 +0900

    fix: delete JPA 메서드 수정

--- src/main/java/com/commerce/pagopa/searchhistory/infrastructure/persistence/SearchHistoryJpaRepository.java
+++ src/main/java/com/commerce/pagopa/searchhistory/infrastructure/persistence/SearchHistoryJpaRepository.java
@@ -3,6 +3,9 @@ package com.commerce.pagopa.searchhistory.infrastructure.persistence;
 import com.commerce.pagopa.searchhistory.domain.model.SearchHistory;
 import com.commerce.pagopa.searchhistory.domain.repository.SearchHistoryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,8 +25,14 @@ public interface SearchHistoryJpaRepository extends JpaRepository<SearchHistory,
     Optional<SearchHistory> findBySessionIdAndKeyword(String sessionId, String keyword);
 
     @Override
-    void deleteByUserId(Long userId);
+    @Modifying
+    @Query("DELETE FROM SearchHistory sh " +
+            "WHERE sh.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 
     @Override
-    void deleteBySessionId(String sessionId);
+    @Modifying
+    @Query("DELETE FROM SearchHistory sh " +
+            "WHERE sh.sessionId = :sessionId")
+    void deleteBySessionId(@Param("sessionId") String sessionId);
 }
