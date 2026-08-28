cee125b3b979b19990a6ef11433c05c7918bfa80
yeoeol <gnsl3528@naver.com>
2026-08-29 01:02:39 +0900

    feat: 검색 기록 엔티티 수정 및 구현

--- src/main/java/com/commerce/pagopa/searchhistory/domain/model/SearchHistory.java
+++ src/main/java/com/commerce/pagopa/searchhistory/domain/model/SearchHistory.java
@@ -1,68 +1,91 @@
 package com.commerce.pagopa.searchhistory.domain.model;
 
-import com.commerce.pagopa.user.domain.model.User;
-import com.commerce.pagopa.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+
+import java.time.Instant;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "search_histories")
-public class SearchHistory extends BaseTimeEntity {
+@Table(
+        name = "search_history",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uq_search_history_user_id_keyword",
+                        columnNames = {"user_id", "keyword"}
+                ),
+                @UniqueConstraint(
+                        name = "uq_search_history_session_id_keyword",
+                        columnNames = {"session_id", "keyword"}
+                ),
+        }
+)
+public class SearchHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "search_history_id")
+    @Column(name = "search_history_id", nullable = false)
     private Long id;
 
     // 비로그인 사용자를 위해 null을 허용
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Column(name = "user_id", nullable = true)
+    private Long userId;
 
     // 비로그인 사용자를 식별하기 위한 세션 ID 또는 디바이스 ID (쿠키, 로컬스토리지 기반)
-    @Column(name = "session_id")
+    @Column(name = "session_id", length = 255, nullable = true)
     private String sessionId;
 
-    @Column(nullable = false, length = 100)
+    @Column(name = "keyword", nullable = false, length = 100)
     private String keyword;
 
     // 검색어 중복 갱신을 위한 전용 시간 필드
     @Column(name = "last_searched_at", nullable = false)
-    private LocalDateTime lastSearchedAt;
+    private Instant lastSearchedAt;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private SearchHistory(User user, String sessionId, String keyword, LocalDateTime lastSearchedAt) {
-        this.user = user;
+    private SearchHistory(
+            Long userId,
+            String sessionId,
+            String keyword,
+            Instant lastSearchedAt
+    ) {
+        this.userId = userId;
         this.sessionId = sessionId;
         this.keyword = keyword;
         this.lastSearchedAt = lastSearchedAt;
     }
 
-    public static SearchHistory createForUser(User user, String keyword) {
+    public static SearchHistory createForUser(
+            Long userId,
+            String keyword,
+            Instant lastSearchedAt
+    ) {
         return SearchHistory.builder()
-                .user(user)
+                .userId(userId)
                 .keyword(keyword)
-                .lastSearchedAt(LocalDateTime.now())
+                .lastSearchedAt(lastSearchedAt)
                 .build();
     }
 
-    public static SearchHistory createForGuest(String sessionId, String keyword) {
+    public static SearchHistory createForGuest(
+            String sessionId,
+            String keyword,
+            Instant lastSearchedAt
+    ) {
         return SearchHistory.builder()
                 .sessionId(sessionId)
                 .keyword(keyword)
-                .lastSearchedAt(LocalDateTime.now())
+                .lastSearchedAt(lastSearchedAt)
                 .build();
     }
 
     // 중복 검색 시 시간 갱신
-    public void updateLastSearchedAt() {
-        this.lastSearchedAt = LocalDateTime.now();
+    public void updateLastSearchedAt(Instant lastSearchedAt) {
+        this.lastSearchedAt = lastSearchedAt;
     }
 }
