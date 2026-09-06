6f0a204f3b44ce9af17b9a490d1d259bed052c82
yeoeol <gnsl3528@naver.com>
2026-09-06 18:27:37 +0900

    refactor: Category 엔티티 toString() 메서드 리팩토링

--- src/main/java/com/commerce/pagopa/category/domain/model/Category.java
+++ src/main/java/com/commerce/pagopa/category/domain/model/Category.java
@@ -64,15 +64,4 @@ public class Category extends BaseTimeEntity {
     public void rename(String name) {
         this.name = name;
     }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", children=" + children +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
 }
