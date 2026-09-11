5d7679f1fa301aad3c53bce653f7f9f9bad11585
yeoeol <gnsl3528@naver.com>
2026-09-11 16:01:18 +0900

    docs: PaymentController Swagger 어노테이션 추가

--- src/main/java/com/commerce/pagopa/payment/presentation/PaymentController.java
+++ src/main/java/com/commerce/pagopa/payment/presentation/PaymentController.java
@@ -18,6 +18,10 @@ import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "PAYMENT API", description = "결제 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payments")
@@ -25,6 +29,7 @@ public class PaymentController {
 
 	private final PaymentService paymentService;
 
+	@Operation(summary = "결제 요청", description = "특정 주문에 대해 결제 수단과 함께 결제를 요청합니다.")
 	@PostMapping("/request")
 	public ResponseEntity<ApiResponse<PaymentResult>> request(
 			@AuthenticationPrincipal(expression = "userId") Long userId,
@@ -35,6 +40,7 @@ public class PaymentController {
 		);
 	}
 
+	@Operation(summary = "결제 승인", description = "요청된 결제에 대해 결제를 승인합니다.")
 	@PostMapping("/approve")
 	public ResponseEntity<ApiResponse<PaymentResult>> approve(
 			@AuthenticationPrincipal(expression = "userId") Long userId,
@@ -45,6 +51,7 @@ public class PaymentController {
 		);
 	}
 
+	@Operation(summary = "결제 취소", description = "결제에 대해 취소를 요청합니다.")
 	@PostMapping("/cancel")
 	public ResponseEntity<ApiResponse<PaymentResult>> cancel(
 			@AuthenticationPrincipal(expression = "userId") Long userId,
